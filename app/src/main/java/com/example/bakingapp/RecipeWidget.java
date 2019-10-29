181407af716ce558e6c8d707589e5324ea56eebf
tan <haitanle88@gmail.com>
2019-10-29 10:48:55 -0400

    displaying hello in widget gridview

--- app/src/main/java/com/example/bakingapp/RecipeWidget.java
+++ app/src/main/java/com/example/bakingapp/RecipeWidget.java
@@ -3,7 +3,11 @@ package com.example.bakingapp;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.widget.RemoteViews;
+
+import com.example.bakingapp.ui.GridWidgetService;
 
 /**
  * Implementation of App Widget functionality.
@@ -13,21 +17,47 @@ public class RecipeWidget extends AppWidgetProvider {
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
-        // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.recipe_widget);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
 
-        // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);
+//        for (int i = 0; i < appWidgetId)
+//
+//        //CharSequence widgetText = context.getString(R.string.appwidget_text);
+//        // Construct the RemoteViews object
+//        //RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.recipe_widget);
+//        //views.setTextViewText(R.id.widget_gridview, widgetText);
+//
+//
+//        //views.setremo;
+//
+//
+//        // Instruct the widget manager to update the widget
+//        appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
+
+            // Set up the intent that starts the StackViewService, which will
+            // provide the views for this collection.
+            Intent intent = new Intent(context, GridWidgetService.class);
+            // Add the app widget ID to the intent extras
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+            intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
+
+            // Instantiate the RemoteViews object for the app widget layout.
+            RemoteViews rv = new RemoteViews(context.getPackageName(), R.layout.recipe_widget);
+
+            rv.setRemoteAdapter(R.id.widget_gridview, intent);
+
+            rv.setEmptyView(R.id.widget_gridview, R.id.empty_view);
+
+            //updateAppWidget(context, appWidgetManager, appWidgetId);
+
+            appWidgetManager.updateAppWidget(appWidgetId, rv);
         }
+
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
     }
 
     @Override
